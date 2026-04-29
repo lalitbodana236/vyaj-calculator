b7b30feb4c7e05cf86d51cca72c8e699d095c517
lalitbodana236 <lalitbodana236@gmail.com>
2026-04-29 20:30:03 +0530

    Ignore node_modules

--- imp.cmd
+++ imp.cmd
@@ -1,3 +1,10 @@
 npm run dev
 npm run build
 npm run deploy
+
+
+echo "node_modules/" >> .gitignore
+git rm -r --cached node_modules
+git add .gitignore
+git commit -m "Ignore node_modules"
+git push
